913329b4ff04b67d7429f1187e84c87cd1187795
rocabadh <rocabadh@dukes.jmu.edu>
2026-09-11 16:42:19 -0400

    Complete 100% HW

--- Module 1 - Language Intro/3 - Conditionals/4 - Assignments/1.3.4.1 - Deviation Lists/deviation_lists_template.py
+++ Module 1 - Language Intro/3 - Conditionals/4 - Assignments/1.3.4.1 - Deviation Lists/deviation_lists_template.py
@@ -22,11 +22,17 @@ random_list_B = random.sample(range(100), random_length)
 
 # use the std() method from numpy to determine which list has the largest standard deviation
 
-### YOUR CODE HERE
+#Finding std() of both lists
+random_list_A=np.std(random_list_A)
+random_list_B=np.std(random_list_B)
 
+if random_list_A > random_list_B:
+    longest_list_is=random_list_A
+else: 
+    longest_list_is=random_list_B
 # set this variable equal to the list with the largest standard deviation
 # do not modify this variable's name, you can/should adjust the contents ;)
 # e.g. longest_list_is = myList
 longest_list_is = None
 
-### YOUR CODE HERE
+
